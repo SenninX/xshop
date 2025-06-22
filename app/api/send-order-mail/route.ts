4066c31756af5839647d003624b55c4fcdaed118
justin <justin@MacBook-Pro-4.local>
2025-06-22 13:48:45 +0900

    StripeのAPIバージョンをダウングレードして動作を再確認

--- app/api/send-order-mail/route.ts
+++ app/api/send-order-mail/route.ts
@@ -1,5 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2023-10-16" as any,
+});
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
